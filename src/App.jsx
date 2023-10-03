08e6d1f66d787a97584d1e7c4b7112d766f030e7
xweeton <xweeton@outlook.com>
2023-10-03 10:41:25 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Zeph Goh</h1>
+      <h2>Software Developer</h2>
+    </>
+  );
+}
+
+export default App;
